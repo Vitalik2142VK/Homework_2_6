f2d4fcad20a818eec0d46e46143df5e2a2804113
Vitalii <lennab@mail.ru>
2023-03-28 10:55:13 +0300

    First commit

---
+++ src/test/java/hw/homework/homework_2_6/Homework26ApplicationTests.java
@@ -0,0 +1,13 @@
+package hw.homework.homework_2_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
