36832ab5b12c082a79f9ee15e68f603b1bb23253
Vitalii <lennab@mail.ru>
2023-04-03 17:12:58 +0300

    ДЗ выполнено

--- src/main/java/hw/homework/homework_2_6/exeptions/EmployeeStorageIsFullException.java
+++ src/main/java/hw/homework/homework_2_6/exeptions/EmployeeStorageIsFullException.java
@@ -1,4 +1,8 @@
 package hw.homework.homework_2_6.exeptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.REQUESTED_RANGE_NOT_SATISFIABLE)
 public class EmployeeStorageIsFullException extends RuntimeException{
 }
